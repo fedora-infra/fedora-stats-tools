5fe393757c8869d43314a101acc62d809d675d49
Remy DeCausemaker <decause@redhat.com>
2015-08-11 13:58:40 -0400

    Updated with Koji queries

--- scripts/datagrepper-queries.py
+++ scripts/datagrepper-queries.py
@@ -1,26 +1,37 @@
-""" stats-for-decause.py
+"""
+query is {'topic': 'org.fedoraproject.prod.buildsys.build.state.change'}
+---------------
+0 years ago 940049
+1 years ago 467118
+2 years ago 133368
+3 years ago 0
 
-so, 0 years ago is august 2014 to august 2015
-1 years ago is august 2013 to august 2014
-2 years ago is august 2012 to august 2013, etc..
-the anonymous comments don't add up back 2 years ago because the
-anonymous format changed in the message payload -- the "contains"
-thing doesn't work exactly that far back.
+query is {'topic': 'org.fedoraproject.prod.buildsys.build.state.change', 'contains': 'primary'}
+---------------
+0 years ago 296830
+1 years ago 121010
+2 years ago 0
+3 years ago 0
 
-Output:
+query is {'topic': 'org.fedoraproject.prod.copr.build.end'}
+---------------
+0 years ago 193176
+1 years ago 64285
+2 years ago 0
+3 years ago 0
 
 query is {'topic': 'org.fedoraproject.prod.bodhi.update.comment'}
 ---------------
-0 years ago 19102
+0 years ago 19098
 1 years ago 16807
-2 years ago 11443
+2 years ago 11448
 3 years ago 0
 
 query is {'topic': 'org.fedoraproject.prod.bodhi.update.comment', 'contains': '"anonymous":false'}
 ---------------
-0 years ago 18732
+0 years ago 18728
 1 years ago 16345
-2 years ago 7140
+2 years ago 7145
 3 years ago 0
 
 query is {'topic': 'org.fedoraproject.prod.bodhi.update.comment', 'contains': '"anonymous":true'}
@@ -28,13 +39,6 @@ query is {'topic': 'org.fedoraproject.prod.bodhi.update.comment', 'contains': '"
 0 years ago 370
 1 years ago 462
 2 years ago 230
-3 years ago 0
-
-query is {'topic': 'org.fedoraproject.prod.copr.build.end'}
----------------
-0 years ago 192937
-1 years ago 64219
-2 years ago 0
 3 years ago 0
 
 """
@@ -63,6 +67,18 @@ def get_count(start, finish, query):
 
 queries = [
     {
+        # You have to divide these results by 2 to get the real number
+        'topic': 'org.fedoraproject.prod.buildsys.build.state.change',
+    },
+    {
+        # You have to divide these results by 2 to get the real number
+        'topic': 'org.fedoraproject.prod.buildsys.build.state.change',
+        'contains': 'primary',
+    },
+    {
+        'topic': 'org.fedoraproject.prod.copr.build.end',
+    },
+    {
         'topic': 'org.fedoraproject.prod.bodhi.update.comment',
     },
     {
@@ -72,9 +88,6 @@ queries = [
     {
         'topic': 'org.fedoraproject.prod.bodhi.update.comment',
         'contains': '"anonymous":true',
-    },
-    {
-        'topic': 'org.fedoraproject.prod.copr.build.end',
     },
 ]
 for query in queries:
