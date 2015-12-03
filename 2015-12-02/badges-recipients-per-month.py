3b611c7aa6b9e2f7ca1040c0ddfd0fbf62ce4c21
Ralph Bean <rbean@redhat.com>
2015-12-02 21:05:12 -0500

    Messing with stuff.

---
+++ 2015-12-02/badges-recipients-per-month.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+import utils
+
+import fedmsg.meta
+import fedmsg.config
+config = fedmsg.config.load_config()
+fedmsg.meta.make_processors(**config)
+
+import logging
+logging.basicConfig(level=logging.DEBUG)
+
+start = 1443074039  # A couple months ago
+
+messages = utils.grep(
+    category='fedbadges',
+    rows_per_page=100,
+    start=start,
+    order='asc',  # Start at the beginning, end at now.
+)
+
+
+users = set()
+last_bucket = None
+for msg in messages:
+    bucket = utils.monthly_timebucket(msg['timestamp'])
+    if not last_bucket:
+        last_bucket = bucket
+    if bucket != last_bucket:
+        # Then we've switched to the next month
+        print last_bucket, len(users)
+        # Reset our markers
+        last_bucket = bucket
+        users = set()
+    users = users.union(fedmsg.meta.msg2usernames(msg, **config))
+    print "#", bucket, len(users)
---
+++ 2015-12-02/utils.py
@@ -0,0 +1,26 @@
+import datetime
+
+import requests
+
+url = 'https://apps.fedoraproject.org/datagrepper/raw'
+
+def grep(**kwargs):
+    response = requests.get(url, params=kwargs)
+    data = response.json()
+    pages = data['pages']
+
+    for message in data['raw_messages']:
+        yield message
+
+    for page in range(1, pages):
+        kwargs['page'] = page
+        response = requests.get(url, params=kwargs)
+        data = response.json()
+        for message in data['raw_messages']:
+            yield message
+
+
+def monthly_timebucket(timestamp):
+    then = datetime.datetime.fromtimestamp(timestamp)
+    date = datetime.date(then.year, then.month, 1)
+    return date
