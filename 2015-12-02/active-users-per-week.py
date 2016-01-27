db94a0118eab4c1ca70b22014db39b9c043d92c2
Ralph Bean <rbean@redhat.com>
2016-01-26 23:35:28 -0500

    Make this work on various topics.

--- 2015-12-02/active-users-per-week.py
+++ 2015-12-02/active-users-per-week.py
@@ -10,8 +10,16 @@ fedmsg.meta.make_processors(**config)
 import time
 import datetime
 import logging
+import os
 import sys
 logging.basicConfig(level=logging.DEBUG)
+
+discriminant = sys.argv[-1]
+if __file__.split('/')[-1] in discriminant:
+    print "usage: '$ ./active-users-per-week.py TOPIC'"
+    sys.exit(1)
+
+print "operating with discriminant", discriminant
 
 verboten = [
     'org.fedoraproject.prod.buildsys.rpm.sign',
@@ -20,14 +28,18 @@ verboten = [
     'org.fedoraproject.prod.buildsys.untag',
 ]
 
+try:
+    os.makedirs('./data/%s' % discriminant)
+except OSError:
+    pass
 
-for day in range(351, 364, 7):
+for day in range(8, 364, 7):
     bucket = "2015-{0:0>3}".format(day - 7)
     terminus = "2015-{0:0>3}".format(day)
     start = int(time.mktime(datetime.datetime.strptime(bucket, "%Y-%j").timetuple()))
     end = int(time.mktime(datetime.datetime.strptime(terminus, "%Y-%j").timetuple()))
 
-    print "Working on data/%s.csv" % bucket
+    print "Working on data/%s/%s.csv" % (discriminant, bucket)
 
     messages = utils.grep(
         rows_per_page=100,
@@ -35,8 +47,9 @@ for day in range(351, 364, 7):
         start=start,
         end=end,
         order='asc',  # Start at the beginning, end at now.
+        topic=discriminant,
         # Cut this stuff out, because its just so spammy.
-        not_user='koschei',
+        not_user=['koschei', 'anonymous'],
         not_topic=verboten,
     )
 
@@ -50,11 +63,11 @@ for day in range(351, 364, 7):
             users[user] = users.get(user, 0) + 1
 
         if i % 50 == 0:
-            print ".",
+            sys.stdout.write(".")
             sys.stdout.flush()
 
     print " done with", bucket
 
-    with open('data/%s.csv' % bucket, 'w') as f:
+    with open('data/%s/%s.csv' % (discriminant, bucket), 'w') as f:
         for user in users:
             f.write('%s, %i\n' % (user, users[user]))
